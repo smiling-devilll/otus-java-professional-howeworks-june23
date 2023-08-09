241f4863c3fe156841e8033d9319388c14ae52e5
v.testova <v.testova@qiwi.com>
2023-08-09 18:55:19 +0300

    hw-02: collections

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES
+
 plugins {
     id("io.spring.dependency-management")
+    id("org.springframework.boot") apply false
 }
 
 
@@ -16,6 +19,9 @@ allprojects {
     apply(plugin = "io.spring.dependency-management")
     dependencyManagement {
         dependencies {
+            imports {
+                mavenBom(BOM_COORDINATES)
+            }
             dependency("com.google.guava:guava:$guava")
         }
     }
@@ -32,6 +38,15 @@ subprojects {
     tasks.withType<JavaCompile> {
         options.encoding = "UTF-8"
         options.compilerArgs.addAll(listOf("-Xlint:all,-serial,-processing"))
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+        testLogging.showExceptions = true
+        reports {
+            junitXml.required.set(true)
+            html.required.set(true)
+        }
     }
 
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,18 @@
 rootProject.name = "otus-java-professional-howeworks-june23"
 
 include("hw01-gradle")
+include("hw02-generics")
 
 pluginManagement {
 
     val johnrengelmanShadow: String by settings
     val dependencyManagement: String by settings
+    val springframeworkBoot: String by settings
 
     plugins {
         id("com.github.johnrengelman.shadow") version johnrengelmanShadow
         id("io.spring.dependency-management") version dependencyManagement
+        id("org.springframework.boot") version springframeworkBoot
+
     }
 }
